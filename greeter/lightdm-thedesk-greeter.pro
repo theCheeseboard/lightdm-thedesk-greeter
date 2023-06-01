ea9272054f83aaeeaac3ba4098bd2509e2a3edd2
Victor Tran <vicr12345@gmail.com>
2023-06-01 18:42:06 +1000

    Move to Qt 6

---
+++ greeter/lightdm-thedesk-greeter.pro
@@ -0,0 +1,79 @@
+QT       += core gui thelib tdesktopenvironment
+SHARE_APP_NAME = lightdm-thedesk-greeter
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++11
+
+# Include the-libs build tools
+include(/usr/share/the-libs/pri/buildmaster.pri)
+
+# The following define makes your compiler emit warnings if you use
+# any Qt feature that has been marked deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can also make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    actionqueue.cpp \
+    differentuserpopover.cpp \
+    main.cpp \
+    mainwindow.cpp \
+    operatingsystemselect.cpp \
+    pamchallengepopover.cpp \
+    passwordpane.cpp \
+    poweroptions.cpp \
+    readypane.cpp \
+    sessionmenu.cpp \
+    userlistpane.cpp
+
+HEADERS += \
+    actionqueue.h \
+    differentuserpopover.h \
+    mainwindow.h \
+    operatingsystemselect.h \
+    pamchallengepopover.h \
+    passwordpane.h \
+    poweroptions.h \
+    readypane.h \
+    sessionmenu.h \
+    userlistpane.h
+
+FORMS += \
+    differentuserpopover.ui \
+    mainwindow.ui \
+    operatingsystemselect.ui \
+    pamchallengepopover.ui \
+    passwordpane.ui \
+    poweroptions.ui \
+    readypane.ui \
+    userlistpane.ui
+
+unix {
+    CONFIG += link_pkgconfig
+    PKGCONFIG += liblightdm-qt5-3
+
+    target.path = /usr/bin
+
+    desktop.files = lightdm-thedesk-greeter.desktop
+    desktop.path = /usr/share/xgreeters
+
+    defaults.files = defaults.conf
+    defaults.path = $$THELIBS_INSTALL_SETTINGS/theSuite/lightdm-thedesk-greeter/
+
+    INSTALLS += target desktop defaults
+}
+
+
+DISTFILES += \
+    CMakeLists.txt \
+    defaults.conf \
+    lightdm-thedesk-greeter.desktop
+
+RESOURCES += \
+    resources.qrc
---
+++ greeter/translations/en_US.ts
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DifferentUserPopover</name>
+    <message>
+        <location filename="../differentuserpopover.ui" line="35"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_differentuserpopover.h" line="86"/>
+        <source>Log In as a different user</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../differentuserpopover.ui" line="59"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_differentuserpopover.h" line="87"/>
+        <source>Who are you logging in as?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../differentuserpopover.ui" line="69"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_differentuserpopover.h" line="88"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../differentuserpopover.ui" line="78"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_differentuserpopover.h" line="89"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="124"/>
+        <location filename="../mainwindow.cpp" line="278"/>
+        <source>That didn&apos;t work</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="125"/>
+        <source>We couldn&apos;t authenticate you.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="166"/>
+        <source>Log In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="166"/>
+        <source>Log in to your device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="279"/>
+        <source>We couldn&apos;t start the authentication process for %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OperatingSystemSelect</name>
+    <message>
+        <location filename="../operatingsystemselect.ui" line="14"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_operatingsystemselect.h" line="93"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../operatingsystemselect.ui" line="35"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_operatingsystemselect.h" line="94"/>
+        <source>Select an operating system</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PowerOptions</name>
+    <message>
+        <location filename="../poweroptions.ui" line="14"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="140"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="35"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="141"/>
+        <source>Power Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="59"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="142"/>
+        <source>Power Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="66"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="143"/>
+        <source>Turns off your computer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="77"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="144"/>
+        <source>Reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="84"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="145"/>
+        <source>Turns off your computer and turns it back on again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="91"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="146"/>
+        <source>Suspend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="98"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="147"/>
+        <source>Puts your computer in a low power state so that it opens up quickly</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="105"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="148"/>
+        <source>Turn Off Screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="112"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="149"/>
+        <source>Turns off the screen, but keeps everything else running in the background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="119"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="150"/>
+        <source>Hibernate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../poweroptions.ui" line="126"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_poweroptions.h" line="151"/>
+        <source>Powers off your computer, but when powered back on, restores the session.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserListPane</name>
+    <message>
+        <location filename="../userlistpane.ui" line="95"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_userlistpane.h" line="177"/>
+        <source>Who&apos;s logging in today?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userlistpane.ui" line="120"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_userlistpane.h" line="178"/>
+        <source>Guest</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userlistpane.ui" line="139"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_userlistpane.h" line="179"/>
+        <source>Someone Else</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../userlistpane.ui" line="169"/>
+        <location filename="../../../build-lightdm-thedesk-greeter-Chroot_6-Debug/greeter/greeter_autogen/include/ui_userlistpane.h" line="180"/>
+        <source>Log In</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
